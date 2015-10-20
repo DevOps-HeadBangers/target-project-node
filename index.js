1c3b7c07654ec977b914d3a2d8ba4b400c2dcb7a
kumar-utsav <kmr.utsav@gmail.com>
2015-10-19 23:11:34 -0400

    Update style

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // Code for Milestone 2
-function inc(p, q) 
-{
+function inc(p, q) {
     if (q == undefined) q = 1; // check for q
-    if (p < 0)    // check for p
+    if (p < 0) // check for p
     {
         p = -p;
     }
