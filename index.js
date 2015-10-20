01c8edf4aba8aff4383f83a5cb0f8bac5e19c454
Kumar Utsav <kmr.utsav@gmail.com>
2015-10-19 23:25:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-'use strict';
+"use strict";
 
 // Code for Milestone 2
 function inc(p, q) {
-    if (q == undefined) q = 1; // check for q
-    if (p < 0)    // check for p
-    {
+    if (q == undefined) {
+        q = 1; // check for q
+    }
+    if (p < 0) {   // check for p
         p = -p;
     }
     return p + q / q;
