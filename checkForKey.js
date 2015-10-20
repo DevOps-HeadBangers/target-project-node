0233c15ef63596a3d077c4464b14b416d4cd929a
kumar-utsav <kmr.utsav@gmail.com>
2015-10-20 18:35:21 -0400

    Update key finding functionality.

--- checkForKey.js
+++ checkForKey.js
@@ -1,35 +1,43 @@
-var dir = require('node-dir');
+var child_process = require('child_process');
+var exec = child_process.exec;
+var fs = require("fs");
 
-dir.readFiles(__dirname, {
-        exclude: ['LICENSE', '.gitignore', '.DS_Store'],
-        excludeDir: ['node_modules', '.git']
-    }, function(err, content, next) {
-        if (err) throw err;
-        var digitalOceanToken = findDigitalOceanToken(String(content));
-        var awsToken = findAwsToken(String(content));
-        if(awsToken != null || digitalOceanToken != null){
-        	console.log("Key in commit. Remove and commit again.");
-        	process.exit(1);
+exec('git diff-index --name-status HEAD -- | cut -c3-', function(err, stdout, stderr) {
+
+    if (err) {
+        console.log('Child process exited with error code', err.code);
+        return
+    }
+
+    var fileNames = stdout.substring(0, stdout.length - 1).split("\n");
+
+    for (var i = 0; i < fileNames.length; i++) {
+        var fileName = fileNames[i];
+        var fileExt = fileName.split('.').pop();
+
+        if (fileExt == 'pem') {
+            console.log("PEM file found. Commit failed. Remove PEM file and commit again.");
+            process.exit(1);
         }
-        next();
-    },
-    function(err, files) {
-        if (err) throw err;
-        for(fileIndex in files){
-            var path = files[fileIndex];
-            var ext = path.split('.').pop();
-            if(ext == 'pem'){
-                console.log("PEM file found. Commit failed. Remove PEM file and commit again.");
+
+        fs.readFile(fileName, 'utf8', function(err, data) {
+            if (err) throw err;
+            var digitalOceanToken = findDigitalOceanToken(String(data));
+            var awsToken = findAwsToken(String(data));
+            if (awsToken != null || digitalOceanToken != null) {
+                console.log("Key in commit. Remove and commit again.");
                 process.exit(1);
             }
-        }
-    });
+        });
 
+    }
 
-function findDigitalOceanToken(content){
-	return content.match(/\"[a-zA-Z0-9]{63,65}\"/);
+});
+
+function findDigitalOceanToken(content) {
+    return content.match(/\"[a-zA-Z0-9]{63,65}\"/);
 }
 
-function findAwsToken(content){
-	return content.match(/\"AKI[a-zA-Z0-9]{17,18}\"/);
+function findAwsToken(content) {
+    return content.match(/\"AKI[a-zA-Z0-9]{17,18}\"/);
 }
