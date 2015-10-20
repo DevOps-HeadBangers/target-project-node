4be9e6e4c3f23f85613e6e2464441e6a98b2af6a
Kumar Utsav <kmr.utsav@gmail.com>
2015-10-19 23:36:34 -0400

    Update test.js

--- test.js
+++ test.js
@@ -2,3 +2,4 @@ var inc = require('./')
 var test = require('tape');
 test('inc', function(t){  t.equal(inc(1,undefined), 2, 'should be equal');   t.end(); })
 test('inc', function(t){  t.equal(inc(1,1), 2, 'should be equal');   t.end(); })
+test('inc', function(t){  t.equal(inc(1,1), 3, 'should be equal');   t.end(); })
